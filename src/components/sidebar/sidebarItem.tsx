0a9f80389a3c81614056c4dc38e60888daf55266
suk-6 <me@suk.kr>
2024-06-20 02:44:48 +0900

    refactor: Update SidebarItem component to include subitems

--- src/components/sidebar/sidebarItem.tsx
+++ src/components/sidebar/sidebarItem.tsx
@@ -1,7 +1,13 @@
 interface SidebarItemProps {
 	title: string;
+	children?: React.ReactNode;
 }
 
-export const SidebarItem = ({ title }: SidebarItemProps) => {
-	return <div className="p-3 hover:bg-gray-200 rounded-lg">{title}</div>;
+export const SidebarItem = ({ title, children }: SidebarItemProps) => {
+	return (
+		<div className=" flex flex-col my-0.5">
+			<div className="p-1.5 hover:bg-gray-200 rounded-lg">{title}</div>
+			<div className=" flex flex-col gap-1">{children}</div>
+		</div>
+	);
 };

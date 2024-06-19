547a6957a43c4e74ab6a8ac5636ad5db54593f98
suk-6 <me@suk.kr>
2024-06-19 14:40:54 +0900

    feat: Add Title component with sidebar toggle functionality

---
+++ src/components/title.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useState } from "react";
+import { CiMenuBurger } from "react-icons/ci";
+import { Sidebar } from "./sidebar";
+export const Title = () => {
+	const [isOpen, setIsOpen] = useState(true);
+
+	return (
+		<div className=" w-full h-16 bg-gray-200 flex flex-row">
+			<CiMenuBurger
+				className=" h-full ml-3"
+				size={40}
+				onClick={() => setIsOpen(!isOpen)}
+			/>
+			{isOpen && <Sidebar />}
+		</div>
+	);
+};

d94036ddc0d28b48d98a116bd0d555f1055f64bd
suk-6 <me@suk.kr>
2024-06-20 02:44:34 +0900

    refactor: Add SidebarSubitem component to Sidebar for displaying subitems

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,13 +1,17 @@
 import styled, { css } from "styled-components";
 import { SidebarItem } from "./sidebarItem";
 import { SidebarSeparator } from "./sidebarSeparator";
+import { SidebarSubitem } from "./sidebarSubitem";
 
 export const Sidebar = () => {
 	return (
 		<div className=" absolute top-[5rem] left-0 w-52 h-[calc(100vh-5rem)] bg-white shadow-lg z-10">
 			<div className=" w-full h-full flex flex-col gap-1 p-4">
 				{[...Array(3)].map((_, i) => (
-					<SidebarItem key={i} title="Home" />
+					<SidebarItem key={i} title="Home">
+						<SidebarSubitem title="aasdf" />
+						<SidebarSubitem title="aasdf" />
+					</SidebarItem>
 				))}
 				<SidebarSeparator />
 				{[...Array(3)].map((_, i) => (

0d8a49b9b87513777f7d8b40c318cd6fcbb3d016
suk-6 <me@suk.kr>
2024-06-19 21:19:09 +0900

    refactor: Extract image slider navigation buttons into separate component

--- src/components/imageSlider/imageSliderButton.tsx
+++ src/components/imageSlider/imageSliderButton.tsx
@@ -3,11 +3,45 @@ import {
 	MdOutlineKeyboardArrowRight,
 } from "react-icons/md";
 
-export const ImageSliderButton = () => {
+interface ImageSliderButtonProps {
+	imageLength: number;
+	currentImage: number;
+	setCurrentImage: (value: number) => void;
+}
+
+export const ImageSliderButton = ({
+	imageLength,
+	currentImage,
+	setCurrentImage,
+}: ImageSliderButtonProps) => {
+	const backwardImage = () => {
+		if (currentImage === 0) {
+			setCurrentImage(imageLength - 1);
+		} else {
+			setCurrentImage(currentImage - 1);
+		}
+	};
+
+	const forwardImage = () => {
+		if (currentImage === imageLength - 1) {
+			setCurrentImage(0);
+		} else {
+			setCurrentImage(currentImage + 1);
+		}
+	};
+
 	return (
 		<div className="absolute top-1/2 transform -translate-y-1/2 w-full flex items-center">
-			<MdOutlineKeyboardArrowLeft size={30} className="ml-1 mr-auto" />
-			<MdOutlineKeyboardArrowRight size={30} className="mr-1 ml-auto" />
+			<MdOutlineKeyboardArrowLeft
+				size={30}
+				className="ml-1 mr-auto"
+				onClick={backwardImage}
+			/>
+			<MdOutlineKeyboardArrowRight
+				size={30}
+				className="mr-1 ml-auto"
+				onClick={forwardImage}
+			/>
 		</div>
 	);
 };

98e7760eae71ff10896101ac473749a8d69dd9ef
suk-6 <me@suk.kr>
2024-06-19 15:12:19 +0900

    feat: Update sidebar styling and layout

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,11 +1,20 @@
 import styled, { css } from "styled-components";
 import { SidebarItem } from "./sidebarItem";
+import { SidebarSeparator } from "./sidebarSeparator";
 
 export const Sidebar = () => {
 	return (
 		<div className=" absolute top-[5rem] left-0 w-52 h-[calc(100vh-5rem)] bg-white">
-			<div className=" w-full h-full flex flex-col gap-2 p-4">
-				{[...Array(10)].map((_, i) => (
+			<div className=" w-full h-full flex flex-col gap-1 p-4">
+				{[...Array(3)].map((_, i) => (
+					<SidebarItem key={i} title="Home" />
+				))}
+				<SidebarSeparator />
+				{[...Array(3)].map((_, i) => (
+					<SidebarItem key={i} title="Home" />
+				))}
+				<SidebarSeparator />
+				{[...Array(5)].map((_, i) => (
 					<SidebarItem key={i} title="Home" />
 				))}
 			</div>

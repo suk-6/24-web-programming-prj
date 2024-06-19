5c2e7a863326285433df27c2277574148b8f35ec
suk-6 <me@suk.kr>
2024-06-19 15:12:12 +0900

    feat: Improve SearchBar component styling and functionality

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -1,11 +1,14 @@
+import { FaSearch } from "react-icons/fa";
+
 export const SearchBar = () => {
 	return (
-		<div className=" w-80 h-full flex items-center overflow-hidden">
+		<div className=" w-80 h-12 rounded-3xl border border-gray-400 bg-gray-50 flex flex-row justify-center overflow-hidden">
 			<input
 				type="text"
-				className=" w-full h-12 p-4 rounded-3xl border border-gray-400 bg-gray-100 text-black outline-none"
+				className=" w-9/12 h-full ml-5 mr-auto bg-gray-50 text-black outline-none"
 				placeholder="검색어를 입력하세요"
 			/>
+			<FaSearch className="h-full mr-4 ml-auto" size={20} />
 		</div>
 	);
 };

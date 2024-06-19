21b933b19d78802abeffc8e5a32f4226ab2ea73d
suk-6 <me@suk.kr>
2024-06-19 20:51:19 +0900

    feat: Add lazy loading for images in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { Title } from "@/components/header/title";
+import { ImageSlider } from "@/components/imageSlider/imageSlider";
 
 export default function Home() {
 	return (
 		<main className=" w-screen h-screen">
-			<div className=" w-full h-full flex">
+			<div className=" w-full h-full flex flex-col">
 				<Title />
+				<div className=" w-full h-full flex justify-center p-10">
+					<ImageSlider />
+				</div>
 			</div>
 		</main>
 	);

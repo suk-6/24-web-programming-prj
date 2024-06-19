fd68cb180c5eda3a2817aa2c615d252d844b690e
suk-6 <me@suk.kr>
2024-06-19 21:19:05 +0900

    feat: Implement lazy loading for images in ImageSlider component

--- src/components/imageSlider/imageSlider.tsx
+++ src/components/imageSlider/imageSlider.tsx
@@ -1,20 +1,30 @@
+"use client";
+
 import Example1 from "@/images/example1.png";
 import Example2 from "@/images/example2.png";
 import { ImageSliderButton } from "./imageSliderButton";
+import { useState } from "react";
 
 export const ImageSlider = () => {
+	const images = [Example1, Example2];
+	const [currentImage, setCurrentImage] = useState<number>(0);
+
 	return (
 		<div className="m-5 w-full h-1/2 bg-gray-200 flex overflow-hidden relative border border-gray-200 rounded-xl shadow-xl">
 			<div
 				style={{
-					backgroundImage: `url(${Example1.src})`,
+					backgroundImage: `url(${images[currentImage].src})`,
 					backgroundSize: "cover",
 					backgroundPosition: "center",
 					width: "100%",
 					height: "100%",
 				}}
 			/>
-			<ImageSliderButton />
+			<ImageSliderButton
+				imageLength={images.length}
+				currentImage={currentImage}
+				setCurrentImage={setCurrentImage}
+			/>
 		</div>
 	);
 };

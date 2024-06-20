c2d3cdedd69e9622150f7b8171958170b0b45d70
suk-6 <me@suk.kr>
2024-06-20 16:27:09 +0900

    refactor: Add automatic image sliding functionality to ImageSlider component

--- src/components/imageSlider/imageSlider.tsx
+++ src/components/imageSlider/imageSlider.tsx
@@ -1,11 +1,18 @@
 "use client";
 import data from "@/data.json";
 import { ImageSliderButton } from "./imageSliderButton";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const ImageSlider = () => {
 	const images = Array.from(data.styles, (style) => style["image"]);
 	const [currentImage, setCurrentImage] = useState<number>(0);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCurrentImage((prev) => (prev + 1) % images.length);
+		}, 3000);
+		return () => clearInterval(interval);
+	}, [currentImage, images.length]);
 
 	return (
 		<div className="m-3 w-full h-full bg-gray-200 flex overflow-hidden relative border border-gray-200 rounded-xl shadow-xl">

9e6889d933eb517bf8c58fd1d488897ebdcd41c8
suk-6 <me@suk.kr>
2024-06-20 15:20:25 +0900

    refactor: Update Sidebar component to adjust height for better display

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -4,7 +4,7 @@ import { SidebarSubitem } from "./sidebarSubitem";
 
 export const Sidebar = () => {
 	return (
-		<div className=" absolute top-[5rem] left-0 w-52 h-[calc(100vh-5rem)] bg-white shadow-lg z-10 select-none">
+		<div className=" absolute top-[5rem] left-0 w-52 h-[calc(100vh-5.125rem)] bg-white shadow-lg z-10 select-none">
 			<div className=" w-full h-full flex flex-col gap-1 p-4">
 				{[...Array(3)].map((_, i) => (
 					<SidebarItem key={i} title="Home">

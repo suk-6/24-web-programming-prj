0891546def741c7f258f1f08d9ae0665221214e9
suk-6 <me@suk.kr>
2024-06-26 15:21:50 +0900

    refactor: Update SidebarItem component to adjust styling and spacing

--- src/components/sidebar/sidebarItem.tsx
+++ src/components/sidebar/sidebarItem.tsx
@@ -6,8 +6,8 @@ interface SidebarItemProps {
 export const SidebarItem = ({ title, children }: SidebarItemProps) => {
 	return (
 		<div className=" flex flex-col my-0.5">
-			<div className="p-1.5 hover:bg-gray-200 rounded-lg">{title}</div>
-			<div className=" flex flex-col gap-1">{children}</div>
+			<div className="p-1.5 rounded-lg">{title}</div>
+			<div className=" flex flex-col gap-2">{children}</div>
 		</div>
 	);
 };

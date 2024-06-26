dd0cd4727ee7f7ffd982f4a3f3fc747c89c96dc6
suk-6 <me@suk.kr>
2024-06-26 15:21:46 +0900

    refactor: Update Sidebar component to display styles and tags dynamically from data.json

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,25 +1,26 @@
 import { SidebarItem } from "./sidebarItem";
 import { SidebarSeparator } from "./sidebarSeparator";
 import { SidebarSubitem } from "./sidebarSubitem";
+import data from "@/data.json";
 
 export const Sidebar = () => {
+	const tags = Array.from(data.styles, (style) => style["tags"]);
+	const uniqueTags = Array.from(new Set(tags.flat()));
+
 	return (
 		<div className=" absolute top-[5rem] left-0 w-52 h-[calc(100vh-5.125rem)] bg-white shadow-lg z-10 select-none">
 			<div className=" w-full h-full flex flex-col gap-1 p-4">
-				{[...Array(3)].map((_, i) => (
-					<SidebarItem key={i} title="Home">
-						<SidebarSubitem title="aasdf" />
-						<SidebarSubitem title="aasdf" />
-					</SidebarItem>
-				))}
+				<SidebarItem title="스타일 별">
+					{data.styles.map((style, i) => (
+						<SidebarSubitem key={i} title={style["title"]} />
+					))}
+				</SidebarItem>
 				<SidebarSeparator />
-				{[...Array(3)].map((_, i) => (
-					<SidebarItem key={i} title="Home" />
-				))}
-				<SidebarSeparator />
-				{[...Array(5)].map((_, i) => (
-					<SidebarItem key={i} title="Home" />
-				))}
+				<SidebarItem title="두상 별">
+					{uniqueTags.map((tag, i) => (
+						<SidebarSubitem key={i} title={tag} />
+					))}
+				</SidebarItem>
 			</div>
 		</div>
 	);

3ac5366c6b0b89097fd42d33e0df104eb4e19268
suk-6 <me@suk.kr>
2024-06-27 14:58:15 +0900

    refactor: Update Title component to include search functionality and state management

--- src/components/header/title.tsx
+++ src/components/header/title.tsx
@@ -4,12 +4,18 @@ import { useState } from "react";
 import { CiMenuBurger } from "react-icons/ci";
 import { Sidebar } from "../sidebar/sidebar";
 import { SearchBar } from "./searchBar";
-export const Title = () => {
+export const Title = ({
+	search,
+	setSearch,
+}: {
+	search: string;
+	setSearch: (value: string) => void;
+}) => {
 	const [isOpen, setIsOpen] = useState<boolean>(false);
 
 	return (
 		<div className=" w-full h-20 bg-white border-b-2 flex flex-row gap-4 items-center p-0.5">
-			{isOpen && <Sidebar />}
+			{isOpen && <Sidebar setSearch={setSearch} />}
 			<div className=" w-12 h-12 ml-3 rounded-lg hover:bg-gray-200">
 				<CiMenuBurger
 					className=" w-full h-full p-2"
@@ -17,7 +23,7 @@ export const Title = () => {
 					onClick={() => setIsOpen(!isOpen)}
 				/>
 			</div>
-			<SearchBar />
+			<SearchBar search={search} setSearch={setSearch} />
 		</div>
 	);
 };

2b400b1ba9653bd3f85ea42584ab03d481b21ca1
suk-6 <me@suk.kr>
2024-06-27 14:58:20 +0900

    refactor: Pass setSearch function to SidebarSubitem component in Sidebar

--- src/components/sidebar/sidebarSubitem.tsx
+++ src/components/sidebar/sidebarSubitem.tsx
@@ -3,13 +3,17 @@ import downRightIcon from "@/icons/downRightIcon.svg";
 
 interface SidebarItemProps {
 	title: string;
+	setSearch: (value: string) => void;
 }
 
-export const SidebarSubitem = ({ title }: SidebarItemProps) => {
+export const SidebarSubitem = ({ title, setSearch }: SidebarItemProps) => {
 	return (
 		<div className=" flex flex-row ml-3">
 			<Image src={downRightIcon} alt="guide" className=" h-3.5" />
-			<div className=" ml-1 w-full hover:bg-gray-200 rounded-lg px-1.5">
+			<div
+				className=" ml-1 w-full hover:bg-gray-200 rounded-lg px-1.5"
+				onClick={() => setSearch(title)}
+			>
 				{title}
 			</div>
 		</div>
